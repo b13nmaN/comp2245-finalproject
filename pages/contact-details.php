35d7ae457e5a8b75efa1e91dadaa3382e6f06a1d
b13nmaN <blenmanjoel@gmail.com>
2023-12-09 07:18:41 -0400

    added navigation to view contact details page

--- pages/contact-details.php
+++ pages/contact-details.php
@@ -1,6 +1,14 @@
 <main>
-    <h2>Contact Details</h2>
-    <!-- header goes here -->
+    <div class="dashboard-header">
+        <h2>Contact Details</h2>
+        <div>
+            <button class="add-contact">Add Contact</button>
+            <button class="add-contact">Add Contact</button>
+        </div>
+        <!-- header goes here -->
+        <!-- do not change the dashboard-header styles -->
+        <!-- change the content within the dashboard-header -->
+    </div>
     <div class="container-main">
         <!-- content goes here -->
     </div>
--- template/header.php
+++ template/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dolphin CRM</title>
     <link rel="stylesheet" href="../assets/css/styles.css">
-    
+    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
 </head>
 
 <body>
