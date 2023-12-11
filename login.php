1938e45a371792369a5fd386cc9be2e12b9b81d4
Joshua Gardiner <joshua.gardiner268@gmail.com>
2023-12-11 17:14:29 -0400

    Added header on Login Page

--- login.php
+++ login.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Login Page</title>
-  </head>
-  <body>
-    <form action="includes/process-login.php" method="post">
-      <h2>Login</h2>
-      <label for="email">Email:</label>
-      <input type="email" id="email" name="email" required />
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" required />
-      <button type="submit">Login</button>
-    </form>
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Login Page</title>
+</head>
+
+<body>
+  <?php include "template/header.php"; ?>
+  <form action="includes/process-login.php" method="post">
+    <h2>Login</h2>
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required />
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required />
+    <button type="submit">Login</button>
+  </form>
+</body>
+
 </html>
