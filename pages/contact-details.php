a5dafadd7fa8baf7979584f8dac0073adc497a25
Garvin Ponde <garvinponde@gmail.com>
2023-12-11 14:56:34 -0400

    added attributes and started styling

--- pages/contact-details.php
+++ pages/contact-details.php
@@ -7,43 +7,52 @@ $notes = loadNotes($conn, $contact_id);
 
 <main>
     <div class="dashboard-header">
-        <h2>Contact Details</h2>
+        <h2><strong><?php echo $currentContact['title'] . "." . $currentContact['firstname'] . " " . $currentContact['lastname']; ?></strong></h2>
+        <div>
+            <p style="color: #666666;"><?php echo "Created on " . date("F j, Y", strtotime($currentContact['created_at'])) . " by " . $currentContact['created_by']; ?></p>
+            <p style="color: #666666;"><?php echo "Updated on " . date("F j, Y", strtotime($currentContact['updated_at'])); ?></p>
+        </div>
         <div>
             <button class="btn-primary to-me">Assign to me</button>
             <button class="btn-primary to-sales-lead">Switch to sales lead</button>
         </div>
-        <!-- header goes here -->
-        <!-- do not change the dashboard-header styles -->
-        <!-- change the content within the dashboard-header -->
-        
     </div>
     <div class="container-main">
-        <!-- content goes here -->
-    </div>
-    <div class="container-main">
-        <div class="notes-container">
+        <div class="contact-info-container" style="margin-bottom: 30px;">
+            <p style="color: #666666;">Email</p>
+            <p><?= $currentContact['email']; ?></p>
+            
+            <p style="color: #666666;">Telephone</p>
+            <p><?= $currentContact['telephone']; ?></p>
+            
+            <p style="color: #666666;">Company</p>
+            <p><?= $currentContact['company']; ?></p>
+            
+            <p style="color: #666666;">Assigned to</p>
+            <p><?= $currentContact['assigned_to']; ?></p>
+        </div>
+        <div class="notes-container" style="margin-bottom: 30px;"> 
             <?php foreach ($notes as $note): 
                 $dateTime = new DateTime($note['created_at']);
                 $formattedDate = $dateTime->format("F j, Y \a\\t g:ia");
-                ?>
-                <div class="note">
-                    <p><?= $note['created_by']; ?></p>
-                    <p><?= $note['comment']; ?></p>
-                    <p><?= $formattedDate; ?></p>
+            ?>
+                <div class="note" style="margin-bottom: 30px;"> 
+                    <p style="margin-bottom: 5px;"><strong><?= $note['created_by']; ?></strong></p>
+                    <p style="margin-bottom: 5px; color: #666666;"><?= $note['comment']; ?></p>
+                    <p style="margin-bottom: 5px; color: #666666;"><?= $formattedDate; ?></p>
                 </div>
             <?php endforeach ?>  
         </div>
         <form action="../includes/process-new-note.php" method="post" id="note-form">
             <input type="hidden" name="contact_id" value="<?php echo $currentContact['id']; ?>">
             <input type="hidden" name="created_by" value="<?php echo $currentContact['firstname']; ?>">
-            <div class="grid-item row-6-span-2">
-                <label for="comment"><?php echo "Add a note about " . $currentContact['firstname']; ?></label>
-                <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
+            <div class="grid-item row-6-span-2" style="margin-bottom: 20px;"> <!-- Added margin-bottom for space -->
+                <label for="comment"><strong><?php echo "Add a note about " . $currentContact['firstname']; ?></strong></label>
+                <textarea name="comment" id="comment" cols="30" rows="10" style="width: 100%; height: 100px; border: 1px solid gray;"></textarea>
             </div>
-                <div class="grid-item row-6-span-2">
+            <div class="grid-item row-6-span-2">
                 <button type="submit" class="save-button">Add Note</button>
             </div>
         </form>
-
     </div>
 </main>
