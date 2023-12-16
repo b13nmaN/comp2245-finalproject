4a9514b540de6c9fc701aa6c9b178113184a9288
b13nmaN <blenmanjoel@gmail.com>
2023-12-16 10:19:43 -0400

    added dashboard-header styles

--- pages/contact-details.php
+++ pages/contact-details.php
@@ -7,12 +7,15 @@ $notes = loadNotes($conn, $contact_id);
 
 <main>
     <div class="dashboard-header">
-        <h2><strong><?php echo $currentContact['title'] . "." . $currentContact['firstname'] . " " . $currentContact['lastname']; ?></strong></h2>
-        <div>
-            <p style="color: #666666;"><?php echo "Created on " . date("F j, Y", strtotime($currentContact['created_at'])) . " by " . $currentContact['created_by']; ?></p>
-            <p style="color: #666666;"><?php echo "Updated on " . date("F j, Y", strtotime($currentContact['updated_at'])); ?></p>
+        <div class="dashboard-header-left">
+            <div class="dashboard-header-title">
+                <h2><strong><?php echo $currentContact['title'] . "." . $currentContact['firstname'] . " " . $currentContact['lastname']; ?></strong></h2>
+                <p style="color: #666666;"><?php echo "Created on " . date("F j, Y", strtotime($currentContact['created_at'])) . " by " . $currentContact['created_by']; ?></p>
+                <p style="color: #666666;"><?php echo "Updated on " . date("F j, Y", strtotime($currentContact['updated_at'])); ?></p>
+            </div>
         </div>
-        <div class="button-contain">
+        
+        <div class="dashboard-header-right">
             <button class="btn-primary to-me">Assign to me</button>
             <button class="btn-primary to-sales-lead">Switch to sales lead</button>
         </div>
