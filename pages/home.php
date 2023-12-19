04a5ff442c3f7c86ae52223a75231c8a9f1cea41
b13nmaN <blenmanjoel@gmail.com>
2023-12-18 23:50:37 -0400

    added access control: display users if admin

--- pages/home.php
+++ pages/home.php
@@ -7,8 +7,6 @@ $filteredArr = getFilterRequest($conn, $sanitizedInput);
 // get all all contacts
 $allContacts = getAllContacts($conn);
 
-var_dump($userData);
-
 if(!isset($_SESSION['user'])) {
     header("Location: /comp2245-finalproject/login.php");
 }
